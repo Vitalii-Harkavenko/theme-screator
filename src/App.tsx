995fb66e5aeb272d6fb75769fd3e0d59c9dd0a44
Vitalii-Harkavenko <vitinfo22@gmail.com>
2023-04-29 20:21:35 +0200

    added json with all tokens

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react"
+
 export const App = () => {
 
+	const [searchValue, setSearchValue] = useState<string>('');
 
-  return (
-    <>
-    <div className="colors-container">
-      <input></input>
-    </div>
-    <div className="canvas"></div>
-    </>
-  )
+    return (
+        <>
+            <div id="bebtra" className="styles-container">
+                <div className="search-container">
+                    <div className="some-svg"></div>
+                    <input
+                        className="search-bar"
+                        type="text"
+                        placeholder="Find a section/text type"
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                    ></input>
+                </div>
+                <input></input>
+            </div>
+            <div className="canvas"></div>
+        </>
+    )
 }
